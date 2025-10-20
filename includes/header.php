67250e0c07de4670aff4b27825ed5b70f6aab23e
Edward Gilbert <egbot@asu.edu>
2025-10-20 08:31:09 -0400

    Configure flora setup

--- includes/header.php
+++ includes/header.php
@@ -1,79 +1,90 @@
-<SCRIPT>
-<!--
-if (top.frames.length!=0)
-  top.location=self.document.location;
-// -->
-</SCRIPT>
-<table id="maintable" cellspacing="0">
-	<tr>
-		<td id="header" colspan="3">
-			<div style="clear:both;">
-				<div style="clear:both;margin-left:auto;margin-right:auto;">
-					<a href="<?php echo $CLIENT_ROOT; ?>/../"><img style="" src="/images/MDEheader-FY23-updated.jpg" /></a>
-				</div>
-			</div>
-			<div id="top_navbar">
-				<div id="right_navbarlinks">
-					<?php
-					if($USER_DISPLAY_NAME){
-						?>
-						<span style="">
-							Welcome <?php echo $USER_DISPLAY_NAME; ?>!
-						</span>
-						<span style="margin-left:5px;">
-							<a href="<?php echo $CLIENT_ROOT; ?>/profile/viewprofile.php">My Profile</a>
-						</span>
-						<span style="margin-left:5px;">
-							<a href="<?php echo $CLIENT_ROOT; ?>/profile/index.php?submit=logout">Logout</a>
-						</span>
-						<?php
-					}
-					else{
-						?>
-						<span style="">
-							<a href="<?php echo $CLIENT_ROOT."/profile/index.php?refurl=".$_SERVER['SCRIPT_NAME']."?".htmlspecialchars($_SERVER['QUERY_STRING'], ENT_QUOTES); ?>">
-								Log In
-							</a>
-						</span>
-						<span style="margin-left:5px;">
-							<a href="<?php echo $CLIENT_ROOT; ?>/profile/newprofile.php">
-								New Account
-							</a>
-						</span>
-						<?php
-					}
+<?php
+if($LANG_TAG == 'en' || !file_exists($SERVER_ROOT.'/content/lang/templates/header.' . $LANG_TAG . '.php'))
+	include_once($SERVER_ROOT . '/content/lang/templates/header.en.php');
+else include_once($SERVER_ROOT . '/content/lang/templates/header.' . $LANG_TAG . '.php');
+$SHOULD_USE_HARVESTPARAMS = $SHOULD_USE_HARVESTPARAMS ?? false;
+$collectionSearchPage = $SHOULD_USE_HARVESTPARAMS ? '/collections/index.php' : '/collections/search/index.php';
+?>
+<div class="header-wrapper">
+	<header>
+		<div class="top-wrapper">
+			<a class="screen-reader-only" href="#end-nav"><?= $LANG['H_SKIP_NAV'] ?></a>
+			<nav class="top-login" aria-label="horizontal-nav">
+				<?php
+				if ($USER_DISPLAY_NAME) {
 					?>
-					<span style="margin-left:5px;margin-right:5px;">
-						<a href='<?php echo $CLIENT_ROOT; ?>/sitemap.php'>Sitemap</a>
+					<div class="welcome-text bottom-breathing-room-rel">
+						<?= $LANG['H_WELCOME'] . ' ' . $USER_DISPLAY_NAME ?>!
+					</div>
+					<span style="white-space: nowrap;" class="button button-tertiary bottom-breathing-room-rel">
+						<a href="<?= $CLIENT_ROOT ?>/profile/viewprofile.php"><?= $LANG['H_MY_PROFILE'] ?></a>
 					</span>
+					<span style="white-space: nowrap;" class="button button-secondary bottom-breathing-room-rel">
+						<a href="<?= $CLIENT_ROOT ?>/profile/index.php?submit=logout"><?= $LANG['H_LOGOUT'] ?></a>
+					</span>
+					<?php
+				} else {
+					?>
+					<span id="login">
+						<form name="loginForm" method="post" action="<?= $CLIENT_ROOT . "/profile/index.php" ?>">
+							<input name="refurl" type="hidden" value="<?= htmlspecialchars($_SERVER['SCRIPT_NAME'], ENT_COMPAT | ENT_HTML401 | ENT_SUBSTITUTE) . "?" . htmlspecialchars($_SERVER['QUERY_STRING'], ENT_QUOTES) ?>">
+							<button class="button button-secondary" name="loginButton" type="submit"><?= $LANG['H_LOGIN'] ?></button>
+						</form>
+					</span>
+					<?php
+				}
+				?>
+			</nav>
+			<div class="top-brand">
+				<!--
+				<a href="<?= $CLIENT_ROOT ?>">
+					<div class="image-container">
+						<img src="<?= $CLIENT_ROOT ?>/images/layout/logo_symbiota.png" alt="Symbiota logo">
+					</div>
+				</a>
+				<div class="brand-name">
+					<h1>Symbiota Brand New Portal</h1>
+					<h2>Redesigned by the Symbiota Support Hub</h2>
 				</div>
-				<ul id="hor_dropdown">
+				-->
+			</div>
+
+		</div>
+		<div class="menu-wrapper">
+			<!-- Hamburger icon -->
+			<input class="side-menu" type="checkbox" id="side-menu" name="side-menu" />
+			<label class="hamb hamb-line hamb-label" for="side-menu" tabindex="0">☰</label>
+			<!-- Menu -->
+			<nav class="top-menu" aria-label="hamburger-nav">
+				<ul class="menu">
 					<li>
-						<a href="<?php echo $CLIENT_ROOT; ?>/../" >Home</a>
+						<a href="<?= $CLIENT_ROOT ?>/index.php">
+							<?= $LANG['H_HOME'] ?>
+						</a>
 					</li>
                     <li>
-                    	<a href="<?php echo $CLIENT_ROOT; ?>/../fauna/collections/harvestparams.php" >Fauna Database</a>
+                    	<a href="https://fauna.madreandiscovery.org/collections/search/index.php" >Fauna Database</a>
                     </li>
 					<li>
-						<a href="<?php echo $CLIENT_ROOT; ?>/collections/index.php" >Search Collections</a>
+						<a href="<?= $CLIENT_ROOT; ?>/collections/search/index.php" >Search Collections</a>
 					</li>
 					<li>
-						<a href="<?php echo $CLIENT_ROOT; ?>/collections/map/index.php" target="_blank">Map Search</a>
+						<a href="<?= $CLIENT_ROOT; ?>/collections/map/index.php" target="_blank">Map Search</a>
 					</li>
 					<li>
-						<a href="<?php echo $CLIENT_ROOT; ?>/imagelib/search.php" >Image Search</a>
+						<a href="<?= $CLIENT_ROOT; ?>/imagelib/search.php" >Image Search</a>
 					</li>
 					<li>
-						<a href="<?php echo $CLIENT_ROOT; ?>/imagelib/index.php" >Browse Images</a>
+						<a href="<?= $CLIENT_ROOT; ?>/imagelib/index.php" >Browse Images</a>
 					</li>
 					<li>
-						<a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php" >Inventories</a>
+						<a href="<?= $CLIENT_ROOT; ?>/projects/index.php" >Inventories</a>
 						<ul>
 							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=74" >Sky Island Provinces</a>
+								<a href="<?= $CLIENT_ROOT; ?>/projects/index.php?pid=74" >Sky Island Provinces</a>
 							</li>
 							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=6" >MABA Inventory Sites</a>
+								<a href="<?= $CLIENT_ROOT; ?>/projects/index.php?pid=6" >MABA Inventory Sites</a>
 							</li>
 						</ul>
 					</li>
@@ -81,16 +92,36 @@ if (top.frames.length!=0)
 						<a href="#" >Interactive Tools</a>
 						<ul>
 							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/checklists/dynamicmap.php?interface=checklist" >Dynamic Checklist</a>
+								<a href="<?= $CLIENT_ROOT; ?>/checklists/dynamicmap.php?interface=checklist" >Dynamic Checklist</a>
 							</li>
 							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/checklists/dynamicmap.php?interface=key" >Dynamic Key</a>
+								<a href="<?= $CLIENT_ROOT; ?>/checklists/dynamicmap.php?interface=key" >Dynamic Key</a>
 							</li>
 						</ul>
 					</li>
+					<li>
+						<a href="<?= $CLIENT_ROOT ?>/includes/usagepolicy.php">
+							<?= $LANG['H_DATA_USAGE'] ?>
+						</a>
+					</li>
+					<li>
+						<a href='<?= $CLIENT_ROOT ?>/sitemap.php'>
+							<?= $LANG['H_SITEMAP'] ?>
+						</a>
+					</li>
+					<!--
+					<li id="lang-select-li">
+						<label for="language-selection"><?= $LANG['H_SELECT_LANGUAGE'] ?>: </label>
+						<select oninput="setLanguage(this)" id="language-selection" name="language-selection">
+							<option value="en">English</option>
+							<option value="es" <?= ($LANG_TAG=='es'?'SELECTED':'') ?>>Español</option>
+							<option value="fr" <?= ($LANG_TAG=='fr'?'SELECTED':'') ?>>Français</option>
+						</select>
+					</li>
+					-->
 				</ul>
-			</div>
-		</td>
-	</tr>
-    <tr>
-		<td id='middlecenter'  colspan="3">
+			</nav>
+		</div>
+		<div id="end-nav"></div>
+	</header>
+</div>
